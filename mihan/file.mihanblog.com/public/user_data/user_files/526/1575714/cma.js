805770039008f71abac57405692f230ae654d853
Mohammadreza Moghimi <mimr9@idzo.midoria.izoko>
2020-12-19 23:06:08 +0330

    remove unrelated blogs from here

--- mihan/file.mihanblog.com/public/user_data/user_files/526/1575714/cma.js
+++
@@ -1,9 +0,0 @@
-function a(){
-document.getElementById('mihan_page_advert').style.display='none';
-}
-
-function b(){
-document.getElementById('js__sabaClick_advert__wrapper').style.display='none';
-}
-setTimeout('b()',1500);
-setTimeout('a()',2500);
--- mihan/file.mihanblog.com/public/user_data/user_files/526/1575714/copyRight.js
+++
@@ -1,7 +0,0 @@
-﻿var pageURL = window.location.href;
-var bol = pageURL.indexOf("ashkboos")
-if(bol!=12)
-{
-document.getElementById('gh').style.display='none';
-alert("شما مجاز به استفاده از این قالب نیستید !");
-}
